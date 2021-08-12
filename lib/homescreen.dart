f239445a1be64f090b642275ab68547f0bdb41d4
malikjee760 <85275592+malikjee760@users.noreply.github.com>
2021-08-12 18:42:21 +0500

    second commit

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_cards/core/constant/images.dart';
+//import 'package:flutter_cards/core/constant/images.dart';
 
 class HomeScreen extends StatefulWidget {
   
@@ -11,56 +11,25 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("cards"),
-      ),
-      body: Card(
-            clipBehavior: Clip.antiAlias,
-            child: Column(
-              children: [
-                ListTile(
-                  leading: Icon(Icons.arrow_drop_down_circle),
-                  title: const Text('Card title 1'),
-                  subtitle: Text(
-                    'Secondary Text',
-                    style: TextStyle(color: Colors.black.withOpacity(0.6)),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Text(
-                    'Greyhound divisively hello coldly wonderfully marginally far upon excluding.',
-                    style: TextStyle(color: Colors.black.withOpacity(0.6)),
-                  ),
-                ),
-                ButtonBar(
-                  alignment: MainAxisAlignment.start,
-                  children: [
-                    ElevatedButton(
-                      
-                      onPressed: () {
-                        // Perform some action
-                      },
-                      child: const Text('ACTION 1'),
-                    ),
-                    ElevatedButton(
-                      onPressed: () {
-                        // Perform some action
-                      },
-                      child: const Text('ACTION 2'),
-                    ),
-                  ],
-                ),
-                // Container(
-                //   height: 200,
-                //   width: 200,
-                //   child:Image.asset(AppImages.abc),
-                //   )
-                
-              ],
-            ),
-          ),
-    );
+        appBar: AppBar(
+          title: Text("Grid View"),
+        ),
+        body: GridView.count(
+          // Create a grid with 2 columns. If you change the scrollDirection to
+          // horizontal, this produces 2 rows.
+          crossAxisCount: 2,
+          // Generate 100 widgets that display their index in the List.
+          children: List.generate(15, (index) {
+            return Center(
+              child: Text(
+                'Item $index',
+                style: Theme.of(context).textTheme.headline5,
+              ),
+            );
+          }),
+        ),
+      );
+    
     
   }
 }
